573b4de30984735c0b6fada0c4ce43a54d716945
dot story <dotstorydot@gmail.com>
2026-08-10 19:50:14 +0300

    Add Meta Pixel 2047933435864199 (Seasons Web) with noscript fallback

--- src/components/marketing/FacebookPixel.tsx
+++ src/components/marketing/FacebookPixel.tsx
@@ -18,8 +18,7 @@ export const FacebookPixel = () => {
 
     useEffect(() => {
         if (!loaded) return;
-
-        // Track pageview on route change
+        // Track pageview on every route change
         window.fbq("track", "PageView");
     }, [pathname, loaded]);
 
@@ -27,25 +26,37 @@ export const FacebookPixel = () => {
 
     return (
         <>
+            {/* Meta Pixel base code */}
             <Script
                 id="fb-pixel"
                 strategy="afterInteractive"
                 onLoad={() => setLoaded(true)}
                 dangerouslySetInnerHTML={{
                     __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${FB_PIXEL_ID}');
-            fbq('track', 'PageView');
-          `,
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '${FB_PIXEL_ID}');
+                        fbq('track', 'PageView');
+                    `,
                 }}
             />
+            {/* noscript fallback for non-JS browsers */}
+            <noscript>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    height="1"
+                    width="1"
+                    style={{ display: "none" }}
+                    src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                    alt=""
+                />
+            </noscript>
         </>
     );
 };
